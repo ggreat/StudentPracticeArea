3b75eacbdcf1eb3fb0300fe78f7dbd8db951a91d
Abjr <yishakabraham2112@gmail.com>
2024-12-05 03:26:10 +0300

    hello

--- GitHubPracticeClass.java
+++ GitHubPracticeClass.java
@@ -23,4 +23,7 @@ public class GitHubPracticeClass {
       public void Kbrom_Mehari(){
         System.out.println("I am Kbrom Mehari");
     }
+    public void abrham(){
+      System.out.println("I am Kbrom Mehari");
+  }
 }
