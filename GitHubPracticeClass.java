4574763c0c7436178df8b00b23dd078b48785478
AwetAraya <167514451+AwetAraya@users.noreply.github.com>
2024-12-11 16:34:24 +0300

    akberet
    Akberet

--- GitHubPracticeClass.java
+++ GitHubPracticeClass.java
@@ -23,4 +23,7 @@ public class GitHubPracticeClass {
       public void Kbrom_Mehari(){
         System.out.println("I am Kbrom Mehari");
     }
+    public void Akberet(){
+        System.out.println("I am Akberet G/tnsae");
+    }
 }
