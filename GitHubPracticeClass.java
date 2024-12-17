e74b4acd74d29769b718ae15fe91165f1cf9a96d
mearg19 <sabahailemariam54@gmail.com>
2024-12-16 23:55:50 -0800

    i added a method that shows my name

--- GitHubPracticeClass.java
+++ GitHubPracticeClass.java
@@ -23,4 +23,7 @@ public class GitHubPracticeClass {
       public void Kbrom_Mehari(){
         System.out.println("I am Kbrom Mehari");
     }
+    public void SabaHaile21(){
+      System.out.println("i am saba");
+  }
 }
